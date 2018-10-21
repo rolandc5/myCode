53ded916a352974546586910f55a1a2768475d88
Roland <rolandcanuto@outlook.com>
2018-10-21 12:47:08 -0700

    learning Ruby

---
+++ ruby.rb
@@ -0,0 +1,17 @@
+# learning ruby
+
+#printing something out in ruby
+#print "Hello Ruby"
+
+#everything is an object in ruby, if you typed print self it will treat it as an object
+#print self;
+
+#printing out something if it's even or odd, number.even? returns either a true or false. the question mark after .even allows that to happen.
+#def odd_or_even(number)
+#return print.even?
+
+def saySomething(str)
+    return str
+end
+
+print saySomething('hello');
